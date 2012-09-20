7bb5de89d977e9924d46d442c501d52cb2efea9c
Curtis Colly <curtiscolly@yahoo.com>
2012-09-19 21:19:49 -0400

    Blank page

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
-"Hello World"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-"Test number 3"
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
+    <style type="text/css"></style>
+  </head>
 
-"Just completed changing the directory structure"
+  <body>
+   <p>Here is another pull test for this page</p>
+   <p>Another test code line for this page</p>
+   <p>Here is yet another line to test this operation</p>
+  </body>
 
+</html>
